7401c166425748532788f8fcbd5772f77c96076f
NickWoodall <82170450+NickWoodall@users.noreply.github.com>
2024-09-16 00:54:28 -0400

    filterdict

--- gudiff_model/PDBDataSet_GraphCon.py
+++ gudiff_model/PDBDataSet_GraphCon.py
@@ -188,10 +188,10 @@ class smallPDBDataset(torch.utils.data.Dataset):
         
         if filter_dict:
             filter_conf = {'allowed_oligomer': ['monomeric'],
-                           'max_loop_percent': 0.75}
+                           'max_loop_percent': 0.5}
             pdb_csv = pdb_csv[pdb_csv.oligomeric_detail.isin(filter_conf['allowed_oligomer'])]
             pdb_csv = pdb_csv[pdb_csv.coil_percent < filter_conf['max_loop_percent']]
-            pdb_csv = pdb_csv[pdb_csv.modeled_seq_len > 900]
+            pdb_csv = pdb_csv[pdb_csv.modeled_seq_len > 45]
             pdb_csv = pdb_csv[pdb_csv.modeled_seq_len < 1000]
             pdb_csv = pdb_csv.sort_values('modeled_seq_len', ascending=False)
             
